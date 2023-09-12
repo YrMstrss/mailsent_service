c7dd354f087a44a3cc9290de064016913ba16dcf
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-12 17:28:38 +0300

    client list

--- client/urls.py
+++ client/urls.py
@@ -1,3 +1,10 @@
-urlpatterns = [
+from django.urls import path
 
+from client.apps import ClientConfig
+from client.views import ClientListView
+
+app_name = ClientConfig.name
+
+urlpatterns = [
+    path('clients/', ClientListView.as_view(), name='client_list')
 ]
--- client/views.py
+++ client/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from client.models import Client
+
+
+class ClientListView(ListView):
+    model = Client
