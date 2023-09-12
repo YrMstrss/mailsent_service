a5d4b8430fbd61dec042078671724b53eef0866d
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-12 15:47:01 +0300

    controller and urls for newsletter list

--- mail/urls.py
+++ mail/urls.py
@@ -1,5 +1,7 @@
+from django.urls import path
 
+from mail.views import NewsletterListView
 
 urlpatterns = [
-
+    path('', NewsletterListView.as_view(), name='newsletter_list'),
 ]
--- mail/views.py
+++ mail/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from mail.models import Newsletter
+
+
+class NewsletterListView(ListView):
+    model = Newsletter
