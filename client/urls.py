429b5560cd7aa175064859413c537a24b798eb73
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-12 17:54:21 +0300

    client update page

--- client/urls.py
+++ client/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from client.apps import ClientConfig
-from client.views import ClientListView, ClientCreateView, ClientDetailView
+from client.views import ClientListView, ClientCreateView, ClientDetailView, ClientUpdateView
 
 app_name = ClientConfig.name
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('clients/', ClientListView.as_view(), name='client_list'),
     path('client/create/', ClientCreateView.as_view(), name='client_create'),
     path('client/<int:pk>/', ClientDetailView.as_view(), name='client_view'),
+    path('client/edit/<int:pk>/', ClientUpdateView.as_view(), name='client_edit')
 ]
--- client/views.py
+++ client/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 
 from client.models import Client
 
 
 class ClientCreateView(CreateView):
+    model = Client
+    fields = ('first_name', 'last_name', 'email', 'comment')
+    success_url = reverse_lazy('client:client_list')
+
+
+class ClientUpdateView(UpdateView):
     model = Client
     fields = ('first_name', 'last_name', 'email', 'comment')
     success_url = reverse_lazy('client:client_list')
