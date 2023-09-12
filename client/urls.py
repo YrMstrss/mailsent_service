91cd55607564fe9b9eb9b47127cc43fe0d863b34
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-12 17:36:11 +0300

    create client page

--- client/urls.py
+++ client/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from client.apps import ClientConfig
-from client.views import ClientListView
+from client.views import ClientListView, ClientCreateView
 
 app_name = ClientConfig.name
 
 urlpatterns = [
-    path('clients/', ClientListView.as_view(), name='client_list')
+    path('clients/', ClientListView.as_view(), name='client_list'),
+    path('client/create/', ClientCreateView.as_view(), name='client_create')
 ]
--- client/views.py
+++ client/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
 from client.models import Client
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ('first_name', 'last_name', 'email', 'comment')
+    success_url = reverse_lazy('client:client_list')
 
 
 class ClientListView(ListView):
