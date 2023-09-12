6712c45e07f3a335b366a9519522141d503aee80
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-12 18:13:05 +0300

    admin page for client

--- client/admin.py
+++ client/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from client.models import Client
+
+
+@admin.register(Client)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name')
+
