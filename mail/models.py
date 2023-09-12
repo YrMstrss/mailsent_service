04da91484259890975468e8c58dd2ed8f5b9a8a7
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-12 15:08:21 +0300

    created new model - newsletter

--- mail/models.py
+++ mail/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Newsletter(models.Model):
+    subject = models.CharField(max_length=150, verbose_name='тема')
+    body = models.TextField(verbose_name='содержание')
+
+    def __str__(self):
+        return f'{self.subject}'
+
+    class Meta:
+        verbose_name = 'рассылка'
+        verbose_name_plural = 'рассылки'
