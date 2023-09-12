41b4b650c8ae10e24e38de9322059ccb6c737335
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-12 17:17:24 +0300

    new model Client

--- client/models.py
+++ client/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=150, verbose_name='имя')
+    last_name = models.CharField(max_length=150, verbose_name='фамилия')
+    email = models.EmailField(verbose_name='почта')
+    comment = models.TextField(verbose_name='комментарий', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.email} ({self.last_name} {self.first_name})'
+
+    class Meta:
+        verbose_name = 'клиент'
+        verbose_name_plural = 'клиенты'
