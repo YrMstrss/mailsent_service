a4946e8165672f01e84496ac14e09e98d49804e0
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-12 16:17:41 +0300

    url and controller for home page

--- mail/urls.py
+++ mail/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from mail.views import NewsletterListView
+from mail.apps import MailConfig
+from mail.views import NewsletterListView, home_page
+
+app_name = MailConfig.name
 
 urlpatterns = [
-    path('', NewsletterListView.as_view(), name='newsletter_list'),
+    path('', home_page, name='home'),
+    path('newsletters/', NewsletterListView.as_view(), name='newsletter_list'),
 ]
--- mail/views.py
+++ mail/views.py
@@ -4,5 +4,9 @@ from django.views.generic import ListView
 from mail.models import Newsletter
 
 
+def home_page(request):
+    return render(request, "mail/home.html")
+
+
 class NewsletterListView(ListView):
     model = Newsletter
