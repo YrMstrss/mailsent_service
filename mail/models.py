6858e5652ca1057d4a202e131b05e01793dbb1ef
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-09-13 17:05:31 +0300

    created model of newsletter settings

--- mail/models.py
+++ mail/models.py
@@ -1,5 +1,35 @@
 from django.db import models
 
+NULLABLE = {'null': True, 'blank': True}
+
+
+class NewsletterSettings(models.Model):
+
+    STATUS_CHOICES = [
+        ('CR', 'Создана'),
+        ('ST', 'Запущена'),
+        ('FI', 'Завершена'),
+
+    ]
+
+    PERIOD_CHOICES = [
+        ('HR', 'Часовая'),
+        ('DL', 'Ежедневная'),
+        ('WK', 'Еженедельная')
+    ]
+
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='CR', verbose_name='статус рассылки')
+    start_time = models.DateTimeField(verbose_name='время начала рассылки', **NULLABLE)
+    finish_time = models.DateTimeField(verbose_name='время окончания рассылки', **NULLABLE)
+    period = models.CharField(max_length=2, choices=PERIOD_CHOICES, verbose_name='периодичность')
+
+    def __str__(self):
+        return f'{self.status}'
+
+    class Meta:
+        verbose_name = 'настройка'
+        verbose_name_plural = 'настройки'
+
 
 class Newsletter(models.Model):
     subject = models.CharField(max_length=150, verbose_name='тема')
