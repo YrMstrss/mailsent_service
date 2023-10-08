b0b6099b7f8fc609afc0d81a30279dacdfe35896
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-08 17:28:45 +0300

    added services

--- mail/services.py
+++ mail/services.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+
 from django.conf import settings
 from django.core.mail import send_mail
+
+from mail.models import Newsletter
 
 
 def send_massage(subject, message, recipients):
@@ -10,3 +14,16 @@ def send_massage(subject, message, recipients):
         recipient_list=recipients,
         fail_silently=False
     )
+
+
+def start_newsletter(newsletter: Newsletter, clients: list):
+    if datetime.now() >= newsletter.settings.finish_time:
+        newsletter.settings.status = 'ST'
+    send_massage(newsletter.subject, newsletter.body, clients)
+    if datetime.now() >= newsletter.settings.finish_time:
+        newsletter.settings.status = 'FI'
+
+
+def get_newsletter_list():
+    letters = list(Newsletter.objects.all())
+    return letters
