f655bf1e756728810081a21420138247e6f726b2
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-10 21:11:34 +0300

    realized verification by email

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView
+from users.views import RegisterView, ProfileView, EmailConfirmationSentView, UserConfirmEmailView, EmailConfirmView
 
 app_name = UsersConfig.name
 
@@ -11,4 +11,8 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
+
+    path('email-sent/', EmailConfirmationSentView.as_view(), name='email_sent'),
+    path('confirm-email/<str:token>/', UserConfirmEmailView.as_view(), name='email_verified'),
+    path('email_verified/', EmailConfirmView.as_view(), name='email_verified'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
+import secrets
+
+from django.conf import settings
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView
+from django.views import View
+from django.views.generic import CreateView, UpdateView, TemplateView
 
 from users.forms import UserRegisterForm, UserProfileChangeForm
 # Create your views here.
@@ -13,6 +18,52 @@ class RegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False
+        token = secrets.token_urlsafe(nbytes=8)
+
+        user.token = token
+        activate_url = reverse_lazy('users:email_verified', kwargs={'token': user.token})
+        send_mail(
+            subject='Подтверждение почты',
+            message=f'Для подтверждения регистрации перейдите по ссылке: '
+                    f'http://localhost:8000/{activate_url}',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[user.email],
+            fail_silently=False
+        )
+        user.save()
+
+        return redirect('users:email_sent')
+
+
+class UserConfirmEmailView(View):
+    def get(self, request, token):
+        user = User.objects.get(token=token)
+
+        user.is_active = True
+        user.token = None
+        user.save()
+        return redirect('users:login')
+
+
+class EmailConfirmationSentView(TemplateView):
+    template_name = 'users/email_sent.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class EmailConfirmView(TemplateView):
+    template_name = 'users/verified.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Ваш электронный адрес активирован'
+        return context
 
 
 class ProfileView(UpdateView):
