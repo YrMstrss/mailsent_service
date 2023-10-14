1b9ae5fade2b167e3c59063b1d20a6c3d55905bd
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-14 16:02:54 +0300

    added a list of clients to whom the newsletter is sent

--- mail/views.py
+++ mail/views.py
@@ -25,6 +25,11 @@ class NewsletterListView(ListView):
 class NewsletterDetailView(DetailView):
     model = Newsletter
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['clients'] = Client.objects.filter(newsletter=self.object)
+        return context
+
 
 class NewsletterCreateView(CreateView):
     model = Newsletter
