b7b252fc564911babd9dfe784b4018c2275ff01e
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-14 16:30:17 +0300

    added 'creator' field for Newsletter model

--- mail/models.py
+++ mail/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 from client.models import Client
@@ -37,9 +38,12 @@ class Newsletter(models.Model):
     subject = models.CharField(max_length=150, verbose_name='тема')
     body = models.TextField(verbose_name='содержание')
 
-    settings = models.ForeignKey(NewsletterSettings, on_delete=models.CASCADE, verbose_name='настройка рассылки')
+    mail_settings = models.ForeignKey(NewsletterSettings, on_delete=models.CASCADE, verbose_name='настройка рассылки')
 
     clients = models.ManyToManyField(Client, verbose_name='клиент')
+
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE,
+                                verbose_name='создатель')
 
     def __str__(self):
         return f'{self.subject}'
