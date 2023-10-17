34db6fa9bc72370431bdd7b7fa88d0f855548c77
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-17 20:00:32 +0300

    created func run_scheduler

--- mail/services.py
+++ mail/services.py
@@ -1,8 +1,13 @@
-from datetime import datetime
+import logging
+from datetime import datetime, timedelta
 
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
 from django.conf import settings
 from django.core.mail import send_mail
+from django_apscheduler.jobstores import DjangoJobStore
 
+from client.models import Client
 from mail.models import Newsletter, NewsletterSettings
 
 
@@ -27,3 +32,51 @@ def start_newsletter(newsletter: Newsletter, clients: list):
         newsletter_settings = NewsletterSettings.objects.filter(newsletter=newsletter)
         newsletter_settings.status = 'FI'
 
+
+logger = logging.getLogger(__name__)
+
+scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
+scheduler.add_jobstore(DjangoJobStore(), "default")
+
+
+def run_scheduler(newsletter: Newsletter):
+
+    clients = Client.objects.filter(newsletter=newsletter)
+    clients_email = [client.email for client in clients]
+
+    if newsletter.mail_settings.start_time:
+        start_time = newsletter.mail_settings.start_time
+    else:
+        start_time = datetime.now()
+
+    if newsletter.mail_settings.finish_time:
+        finish_time = newsletter.mail_settings.finish_time
+    else:
+        finish_time = start_time + timedelta(days=365)
+
+    if newsletter.mail_settings.period == 'HR':
+        trigger = CronTrigger(second=start_time.second)
+        # trigger = CronTrigger(second=start_time.second,
+        #                       minute=start_time.minute,
+        #                       start_date=start_time, end_date=finish_time)
+    elif newsletter.mail_settings.period == 'DL':
+        trigger = CronTrigger(second=start_time.second,
+                              minute=start_time.minute,
+                              hour=start_time.hour,
+                              start_date=start_time, end_date=finish_time)
+    else:
+        trigger = CronTrigger(second=start_time.second,
+                              minute=start_time.minute,
+                              hour=start_time.hour,
+                              day_of_week=start_time.weekday(),
+                              start_date=start_time, end_date=finish_time)
+
+    scheduler.add_job(
+        start_newsletter,
+        kwargs={'newsletter': newsletter, 'clients': clients_email},
+        trigger=trigger,
+        id=f"start_newsletter {newsletter} ({newsletter.pk})",  # The `id` assigned to each job MUST be unique
+        max_instances=1,
+        replace_existing=True,
+    )
+    logger.info(f"Added job '{id}'.")
