cd04b31bac8d307fc2aa04806e2581ec7bb90dd9
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-17 13:14:18 +0300

    added default value for 'view_counter'

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=150, verbose_name='заголовок')
     body = models.TextField(verbose_name='содержимое')
     picture = models.ImageField(upload_to='blog/', verbose_name='изображение', **NULLABLE)
-    view_counter = models.IntegerField(verbose_name='количество просмотров')
+    view_counter = models.IntegerField(default=0, verbose_name='количество просмотров')
     created_at = models.DateTimeField(auto_now=True, verbose_name='дата публикации')
 
     def __str__(self):
