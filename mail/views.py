f6b17bd3e6dde35dc11419b70d45c66061badde4
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-17 14:51:30 +0300

    added docstrings, view of active newsletter counter and 3 random blog

--- mail/views.py
+++ mail/views.py
@@ -1,21 +1,46 @@
+import random
+
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, DeleteView, UpdateView, TemplateView
 
+from blog.models import Blog
 from client.models import Client
 from mail.forms import NewsletterForm, NewsletterSettingsForm
 from mail.models import Newsletter, NewsletterSettings
 
 
 class home_page(TemplateView):
+    """
+    Контроллер домашней страницы
+    """
     template_name = 'mail/home.html'
 
     def get_context_data(self, **kwargs):
+        """
+        Метод получающий контекстную информацию, выводимую на домашней странице (общее количество рассылок, число
+        активных рассылок, число клиентов и 3 случайные статьи блога)
+        """
         context = {}
         newsletters = Newsletter.objects.all().count()
+
+        active_newsletters = 0
+        for newsletter in Newsletter.objects.all():
+            if newsletter.mail_settings.status == 'ST':
+                active_newsletters += 1
+
         clients = Client.objects.all().count()
+
+        if Blog.objects.all().count() > 3:
+            blog_numbers = [random.randint(0, Blog.objects.all().count() - 1) for _ in range(3)]
+            for i in range(3):
+                context[f'blog{i}'] = Blog.objects.all()[blog_numbers[i]]
+        else:
+            for i in range(Blog.objects.all().count()):
+                context[f'blog{i}'] = Blog.objects.all()[i]
+
         context['newsletters'] = newsletters
+        context['active_newsletters'] = active_newsletters
         context['clients'] = clients
         if self.request.user.has_perm('users.view_user_list_user'):
             context['users'] = 'Пользователи'
