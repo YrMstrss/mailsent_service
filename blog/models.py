2293f86351b3ed39d3388c1b8536facbfbb3347f
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-17 13:08:37 +0300

    created 'blog' model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Blog(models.Model):
+
+    title = models.CharField(max_length=150, verbose_name='заголовок')
+    body = models.TextField(verbose_name='содержимое')
+    picture = models.ImageField(upload_to='blog/', verbose_name='изображение', **NULLABLE)
+    view_counter = models.IntegerField(verbose_name='количество просмотров')
+    created_at = models.DateTimeField(auto_now=True, verbose_name='дата публикации')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+
+        verbose_name = 'статья блога'
+        verbose_name_plural = 'статьи блога'
