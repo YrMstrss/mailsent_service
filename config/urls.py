6f57bc88e79836dfbc5a325751bed420d5edbab9
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-17 13:03:00 +0300

    created urls for blog

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mail.urls')),
     path('', include('client.urls')),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
+    path('blog/', include('blog.urls'))
 ]
