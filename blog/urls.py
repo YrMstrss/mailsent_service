783f76a5024738adcbe37f5d087feba374283703
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-17 13:56:06 +0300

    added url for blog detail page

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import BlogListView
+from blog.views import BlogListView, BlogDetailView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
     path('list/', BlogListView.as_view(), name='blog_list'),
+    path('<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
 ]
