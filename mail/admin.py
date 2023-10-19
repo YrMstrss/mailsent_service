a9a01dc126baccc24952bb2a7ced411e591ce48f
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-10-19 18:02:41 +0300

    added Logs in admin page

--- mail/admin.py
+++ mail/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from mail.models import Newsletter, NewsletterSettings
+from mail.models import Newsletter, NewsletterSettings, NewsletterLogs
 
 
 @admin.register(Newsletter)
@@ -12,4 +12,9 @@ class NewsletterAdmin(admin.ModelAdmin):
 @admin.register(NewsletterSettings)
 class NewsletterSettingsAdmin(admin.ModelAdmin):
     list_display = ('pk', 'status', 'start_time', 'finish_time', 'period')
+
+
+@admin.register(NewsletterLogs)
+class NewsletterLogsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'last_try', 'server_answer', 'status', 'newsletter')
 
